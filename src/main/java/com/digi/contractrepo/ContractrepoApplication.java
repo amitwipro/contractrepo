4c621a92684ee7e2e59645e7d6ee5b1972d612b0
amitaucs <the.amisoft@gmail.com>
2020-05-10 18:24:23 +0200

    First Commit

---
+++ src/main/java/com/digi/contractrepo/ContractrepoApplication.java
@@ -0,0 +1,13 @@
+package com.digi.contractrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractrepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digi/contractrepo/ContractrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digi.contractrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractrepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
